98e95437654dc483f9e56479c65fc7f7b9bdb96b
blindlobstar <boryapictures@gmail.com>
2024-12-29 21:19:11 +0100

    healthchecks

--- main.go
+++ main.go
@@ -139,6 +139,7 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("GET /upload-form", uploadFormHandler)
 	http.Handle("GET /metrics", promhttp.Handler())
+	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
 
 	slog.Info("Server started on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
