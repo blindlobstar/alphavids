8274455c6f65df31d1bc10d28a753d6db0080ee4
blindlobstar <boryapictures@gmail.com>
2024-12-29 19:30:33 +0100

    metrics

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"io"
 	"io/fs"
 	"log"
 	"log/slog"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
@@ -13,6 +16,14 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	api "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 const (
@@ -21,6 +32,10 @@ const (
 
 var templates *template.Template
 var notFoundFile, notFoundErr = http.Dir("dummy").Open("does-not-exist")
+var meter = otel.Meter("github.com/blindlobstar/alphavids")
+var transcodeErrCounter api.Int64Counter
+var transcodeSuccessCounter api.Int64Counter
+var transcodeHistogram api.Int64Histogram
 
 type noDirFS struct {
 	http.Dir
@@ -43,6 +58,61 @@ func (m noDirFS) Open(name string) (result http.File, err error) {
 }
 
 func main() {
+	var err error
+	res, err := resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName("alphavids"),
+			semconv.ServiceVersion("0.0.1"),
+		))
+	if err != nil {
+		slog.Error("error creating resource", "error", err)
+		return
+	}
+
+	exporter, err := prometheus.New()
+	if err != nil {
+		slog.Error("error creating exporter", "error", err)
+		return
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(exporter),
+	)
+
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		if err := meterProvider.Shutdown(context.Background()); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	otel.SetMeterProvider(meterProvider)
+
+	transcodeErrCounter, err = meter.Int64Counter("transcode.error.count",
+		api.WithDescription("Number of video transcoded fails"),
+		api.WithUnit("{video}"))
+	if err != nil {
+		slog.Error("error creating metrics", "error", err)
+		return
+	}
+
+	transcodeSuccessCounter, err = meter.Int64Counter("transcode.count",
+		api.WithDescription("Number of video transcoded"),
+		api.WithUnit("{video}"))
+	if err != nil {
+		slog.Error("error creating metrics", "error", err)
+		return
+	}
+
+	transcodeHistogram, err = meter.Int64Histogram("transcode.duration",
+		api.WithDescription("Duration of video transcode"),
+		api.WithUnit("ms"))
+	if err != nil {
+		slog.Error("error creating metrics", "error", err)
+		return
+	}
+
 	if err := os.MkdirAll(videos_path, os.ModeDir); err != nil {
 		slog.Error("error creating path for storing videos", "error", err)
 		return
@@ -57,7 +127,6 @@ func main() {
 		}
 	}()
 
-	var err error
 	templates, err = template.ParseGlob("templates/*.html")
 	if err != nil {
 		slog.Error("error parsing templates", "error", err)
@@ -67,10 +136,14 @@ func main() {
 	http.HandleFunc("POST /transcode", transcodeHandler)
 	http.Handle("GET /videos/", http.StripPrefix("/videos/", http.FileServer(noDirFS{http.Dir(videos_path)})))
 	http.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(noDirFS{http.Dir("./static")})))
-	http.HandleFunc("GET /", indexHandler)
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("GET /upload-form", uploadFormHandler)
+	http.Handle("GET /metrics", promhttp.Handler())
+
 	slog.Info("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		slog.Error("error running server", "error", err)
+	}
 }
 
 func deleteOldFiles(path string) error {
@@ -129,20 +202,35 @@ func transcodeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	start := time.Now()
+	fpath, err := transcodeWebmToMOV(file, fileHeader.Filename)
+	if err != nil {
+		transcodeErrCounter.Add(r.Context(), 1)
+		slog.Error("error transcoding file", "error", err)
+		writeResponse(w, http.StatusOK, "Something went wrong. Please try again later")
+		return
+	}
+	transcodeSuccessCounter.Add(r.Context(), 1)
+	duration := time.Since(start)
+	transcodeHistogram.Record(r.Context(), duration.Milliseconds())
+
+	w.WriteHeader(http.StatusOK)
+	if err := templates.ExecuteTemplate(w, "video-ready", fpath); err != nil {
+		slog.Error("error executing template", "error", err)
+	}
+}
+
+func transcodeWebmToMOV(file multipart.File, name string) (string, error) {
 	buf, err := io.ReadAll(file)
 	if err != nil {
-		slog.Error("error reading file into buffer", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error reading file into buffer: %w", err)
 	}
 
 	dir, err := os.MkdirTemp(videos_path, "*")
 	if err != nil {
-		slog.Error("error creating tempdir", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error creating tempdir: %w", err)
 	}
-	fpath := path.Join(dir, strings.TrimSuffix(fileHeader.Filename, filepath.Ext(fileHeader.Filename))+".mov")
+	fpath := path.Join(dir, strings.TrimSuffix(name, filepath.Ext(name))+".mov")
 
 	cmd := exec.Command("ffmpeg",
 		"-vcodec", "libvpx-vp9",
@@ -163,39 +251,25 @@ func transcodeHandler(w http.ResponseWriter, r *http.Request) {
 
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		slog.Error("error creating stdin pipe", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error creating stdin pipe: %w", err)
 	}
 
 	if err := cmd.Start(); err != nil {
-		slog.Error("error starting command", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error starting command: %w", err)
 	}
 
 	if _, err := stdin.Write(buf); err != nil {
-		slog.Error("error writing buffer into stdin pipe", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error writing buffer into stdin pipe: %w", err)
 	}
 
 	if err := stdin.Close(); err != nil {
-		slog.Error("error closing stdin pipe", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error closing stdin pipe: %w", err)
 	}
 
 	if err := cmd.Wait(); err != nil {
-		slog.Error("error waiting for command to exit", "error", err)
-		writeResponse(w, http.StatusInternalServerError, "Something went wrong. Please try again later")
-		return
+		return "", fmt.Errorf("error waiting for command to exit: %w", err)
 	}
-
-	w.WriteHeader(http.StatusOK)
-	if err := templates.ExecuteTemplate(w, "video-ready", fpath); err != nil {
-		slog.Error("error executing template", "error", err)
-	}
+	return fpath, nil
 }
 
 type Form struct {
